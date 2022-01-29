41185aa9c103105513b1cf5401b8fa2c95eddeb2
yano1978 <yano1978@gmail.com>
2022-01-29 12:30:35 +0000

    Edit text refactor w/ plugin + knobs text modification WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
-import { useState, useRef, defaultState } from 'react';
-import Editable from './components/Editable';
-
+import { useState } from 'react';
+import { EditText } from 'react-edit-text';
+import 'react-edit-text/dist/index.css';
 function App() {
-  const inputRef = useRef();
-  const [font, setFont] = useState('');
-  const [slide, setSlide] = useState();
+  const [size, setSize] = useState();
   const [select, setSelect] = useState();
+  const [height, setHeight] = useState();
+  const [space, setSpacing] = useState();
 
-  const handleChange = (e) => {
-    console.log('setting level', e.target.value);
-    setSlide(e.target.value);
+  const fontsizeChange = (e) => {
+    // console.log('setting font size', e.target.value);
+    setSize(e.target.value);
   };
-  const selectChange = (e) => {
-    console.log('setting select', e.target.value);
+  const selectWeightChange = (e) => {
+    // console.log('setting select', e.target.value);
     setSelect(e.target.value);
+  };
+  const selectHeightChange = (e) => {
+    // console.log('setting font height', e.target.value);
+    setHeight(e.target.value);
+  };
+  const selectSpacingChange = (e) => {
+    // console.log('setting font spacing', e.target.value);
+    setSpacing(e.target.value);
   };
   return (
     <>
       <div className="flex flex-row w-full mt-10 mx-2">
         <div className="grid flex-grow h-20 place-items-center">
           <span>Text size</span>
-          {slide}
           <input
-            onChange={(e) => console.log(e.target.value)}
-            onMouseUp={handleChange}
+            onMouseUp={fontsizeChange}
             type="range"
+            min="10"
             max="100"
             className="range"
           />
@@ -32,54 +39,58 @@ function App() {
         <div className="divider divider-vertical"></div>
         <div className="grid flex-grow h-20 place-items-center">
           <span>Leading</span>
-          <input type="range" max="100" className="range" />
+          <input
+            onChange={selectHeightChange}
+            type="range"
+            max="100"
+            className="range"
+          />
         </div>
       </div>
       <div className="flex flex-row w-full mt-10 mx-2 pb-6">
         <div className="grid flex-grow h-20 place-items-center">
           <span>Select weight</span>
           <select
-            onChange={selectChange}
+            onChange={selectWeightChange}
             className="select select-bordered select-sm w-full px-4"
           >
-            <option disabled="disabled" selected="selected">
-              Select weight
-            </option>
+            <option disabled="disabled">Select weight</option>
             <option>100</option>
             <option>200</option>
             <option>300</option>
             <option>400</option>
             <option>500</option>
             <option>600</option>
+            <option>700</option>
+            <option>800</option>
+            <option>900</option>
           </select>
         </div>
         <div className="divider divider-vertical"></div>
         <div className="grid flex-grow h-20 place-items-center">
           <span>Tracking</span>
-          <input type="range" max="100" className="range" />
+          <input
+            onMouseUp={selectSpacingChange}
+            type="range"
+            max="100"
+            className="range"
+          />
         </div>
       </div>
       <div className="flex flex-row w-full mt-10 mx-2">
         <div className="grid flex-grow h-20 place-items-center">
-          <span style={{ fontSize: slide, fontWeight: select }}>Test</span>
-          <Editable
-            text={font}
+          <EditText
+            name="textbox"
+            style={{
+              fontSize: size,
+              fontWeight: select,
+              height: height,
+              letterSpacing: space,
+            }}
+            className="text-edit outline-0 bg-base-100 text-center text-4xl"
+            defaultValue="Some text here"
             placeholder="Some text here"
-            childRef={inputRef}
-            type="input"
-            style={{ fontSize: slide, fontWeight: select }}
-          >
-            <input
-              style={{ fontSize: slide, fontWeight: select }}
-              ref={inputRef}
-              type="text"
-              name="font"
-              className="outline-0 bg-base-100 text-4xl"
-              placeholder="Some text here"
-              value={font}
-              onChange={(e) => setFont(e.target.value)}
-            />
-          </Editable>
+          />
         </div>
       </div>
     </>
